45475f7ff145536c72eb4b97c157235afceeaeed
borafirlangec <borafirlangec@gmail.com>
2019-03-30 12:55:51 +0300

    Ana dosyalar Atıldı
    Bu dosyalar, çevirisi yapılması gereken dosyalardır. Bunlar üzerinde çalışacaklar, kendi branch-ları üzerinde çalışmalıdır. tamamlanan Branch-lar daha sonra Master Branch-ına eklenecektir.

---
+++ 08-stewardess/2 - Script/amicrophone.bat
@@ -0,0 +1,68 @@
+copy /b cont1.mp3 + close.mp3 cont1.mp3
+copy /b cont2.mp3 + close.mp3 cont2.mp3
+copy /b cont3.mp3 + close.mp3 cont3.mp3
+copy /b cont4.mp3 + close.mp3 cont4.mp3
+copy /b cont5.mp3 + close.mp3 cont5.mp3
+copy /b cont6.mp3 + close.mp3 cont6.mp3
+copy /b sea.mp3 + close.mp3 sea.mp3
+copy /b stew1.mp3 + close.mp3 stew1.mp3
+copy /b stew2.mp3 + close.mp3 stew2.mp3
+copy /b stew3.mp3 + close.mp3 stew3.mp3
+copy /b stew4.mp3 + close.mp3 stew4.mp3
+copy /b stew5.mp3 + close.mp3 stew5.mp3
+copy /b storm-after.mp3 + close.mp3 storm-after.mp3
+copy /b storm-before.mp3 + close.mp3 storm-before.mp3
+copy /b turbulence-after.mp3 + close.mp3 turbulence-after.mp3
+copy /b turbulence-before.mp3 + close.mp3 turbulence-before.mp3
+copy /b 1.mp3 + close.mp3 1.mp3
+del close.mp3
+copy /b open.mp3 + cont1.mp3 cont1.mp3a
+del cont1.mp3
+ren cont1.mp3a cont1.mp3
+copy /b open.mp3 + cont2.mp3 cont2.mp3a
+del cont2.mp3
+ren cont2.mp3a cont2.mp3
+copy /b open.mp3 + cont3.mp3 cont3.mp3a
+del cont3.mp3
+ren cont3.mp3a cont3.mp3
+copy /b open.mp3 + cont4.mp3 cont4.mp3a
+del cont4.mp3
+ren cont4.mp3a cont4.mp3
+copy /b open.mp3 + cont5.mp3 cont5.mp3a
+del cont5.mp3
+ren cont5.mp3a cont5.mp3
+copy /b open.mp3 + cont6.mp3 cont6.mp3a
+del cont6.mp3
+ren cont6.mp3a cont6.mp3
+copy /b open.mp3 + sea.mp3 sea.mp3a
+del sea.mp3
+ren sea.mp3a sea.mp3
+copy /b open.mp3 + stew1.mp3 stew1.mp3a
+del stew1.mp3
+ren stew1.mp3a stew1.mp3
+copy /b open.mp3 + stew2.mp3 stew2.mp3a
+del stew2.mp3
+ren stew2.mp3a stew2.mp3
+copy /b open.mp3 + stew3.mp3 stew3.mp3a
+del stew3.mp3
+ren stew3.mp3a stew3.mp3
+copy /b open.mp3 + stew4.mp3 stew4.mp3a
+del stew4.mp3
+ren stew4.mp3a stew4.mp3
+copy /b open.mp3 + stew5.mp3 stew5.mp3a
+del stew5.mp3
+ren stew5.mp3a stew5.mp3
+copy /b open.mp3 + storm-after.mp3 storm-after.mp3a
+del storm-after.mp3
+ren storm-after.mp3a storm-after.mp3
+copy /b open.mp3 + storm-before.mp3 storm-before.mp3a
+del storm-before.mp3
+ren storm-before.mp3a storm-before.mp3
+copy /b open.mp3 + turbulence-after.mp3 turbulence-after.mp3a
+del turbulence-after.mp3
+ren turbulence-after.mp3a turbulence-after.mp3
+copy /b open.mp3 + turbulence-before.mp3 turbulence-before.mp3a
+del turbulence-before.mp3
+ren turbulence-before.mp3a turbulence-before.mp3
+del open.mp3
+del microphone.bat
---
+++ 09-Grounds/Script/akey.bat
@@ -0,0 +1,34 @@
+copy /b 1.mp3 + akey.mp3 1.mp3
+copy /b 2.mp3 + akey.mp3 2.mp3
+copy /b 3.mp3 + akey.mp3 3.mp3
+copy /b a1.mp3 + akey.mp3 a1.mp3
+copy /b a2.mp3 + akey.mp3 a2.mp3
+copy /b a3.mp3 + akey.mp3 a3.mp3
+copy /b a4.mp3 + akey.mp3 a4.mp3
+copy /b a5.mp3 + akey.mp3 a5.mp3
+copy /b b1.mp3 + akey.mp3 b1.mp3
+copy /b b2.mp3 + akey.mp3 b2.mp3
+copy /b b3.mp3 + akey.mp3 b3.mp3
+copy /b b4.mp3 + akey.mp3 b4.mp3
+copy /b b5.mp3 + akey.mp3 b5.mp3
+copy /b welcome1.mp3 + akey.mp3 welcome1.mp3
+copy /b welcome2.mp3 + akey.mp3 welcome2.mp3
+copy /b norequest.mp3 + akey.mp3 norequest.mp3
+copy /b rolltogate11.mp3 + akey.mp3 rolltogate11.mp3
+copy /b rolltogate21.mp3 + akey.mp3 rolltogate21.mp3
+copy /b rolltogate31.mp3 + akey.mp3 rolltogate31.mp3
+copy /b rolltogate41.mp3 + akey.mp3 rolltogate41.mp3
+copy /b rolltogate12.mp3 + akey.mp3 rolltogate12.mp3
+copy /b rolltogate22.mp3 + akey.mp3 rolltogate22.mp3
+copy /b rolltogate32.mp3 + akey.mp3 rolltogate32.mp3
+copy /b rolltogate42.mp3 + akey.mp3 rolltogate42.mp3
+copy /b wait1.mp3 + akey.mp3 wait1.mp3
+copy /b wait2.mp3 + akey.mp3 wait2.mp3
+copy /b stop.mp3 + akey.mp3 stop.mp3
+copy /b rolltopark1.mp3 + akey.mp3 rolltopark1.mp3
+copy /b rolltopark2.mp3 + akey.mp3 rolltopark2.mp3
+copy /b rolltopark3.mp3 + akey.mp3 rolltopark3.mp3
+copy /b bye.mp3 + akey.mp3 bye.mp3
+del akey.mp3
+del akey.bat
+exit
